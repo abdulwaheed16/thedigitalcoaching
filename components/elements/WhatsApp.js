a8dbe88802febee52c41305cbefe51e0d7fc5a73
abdulwaheed16 <abwaheed.ahmed@gmail.com>
2025-01-13 05:22:51 +0500

    whatsapp contact updated

--- components/elements/WhatsApp.js
+++ components/elements/WhatsApp.js
@@ -1,10 +1,11 @@
+import { siteConfig } from "@/config/site";
 import Link from "next/link";
 import React from "react";
 
 const WhatsApp = () => {
   return (
     <div className="whatsapp open">
-      <Link href={`https://wa.me/+923120012250`} target="_blank">
+      <Link href={`https://wa.me/${siteConfig?.phone}`} target="_blank">
         <img src="/assets/img/icons/whatsapp.png" alt="whatsapp" />
       </Link>
       {/* <div className="whatsapp-text">whatsapp</div> */}
